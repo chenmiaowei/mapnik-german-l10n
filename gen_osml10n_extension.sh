7a352020896bb28d25d365b906c072294ac04bc6
chenmiaowei <15888660186@163.com>
2022-01-05 14:35:28 +0400

    Update gen_osml10n_extension.sh

--- gen_osml10n_extension.sh
+++ gen_osml10n_extension.sh
@@ -20,7 +20,7 @@ done
 if ! [ -f "country_osm_grid.sql" ]; then
   rm -f country_osm_grid.sql
   echo -n "Trying to download country_grid.sql.gz from nominatim.org... "
-  curl -s http://www.nominatim.org/data/country_grid.sql.gz |gzip -d >country_osm_grid.sql
+  curl -s https://github.com/chenmiaowei/Resource/raw/main/country_grid.sql.gz |gzip -d >country_osm_grid.sql
 
   if ! [ -s country_osm_grid.sql ]; then
     rm -f country_osm_grid.sql
